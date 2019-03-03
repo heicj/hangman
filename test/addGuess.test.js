0c06ef533c5cb28291fcd697c872b43b7129b124
Charlie <heinercj@gmail.com>
2019-03-03 11:22:42 -0800

    got mocha to work by adding moch-jsdom jsdom and jsdom-global

--- test/addGuess.test.js
+++ test/addGuess.test.js
@@ -1,8 +1,18 @@
 const assert = require('assert');
+const jsdom = require('mocha-jsdom')
+const correctGuesses = require('../src/correctGuesses')
 
 const addGuess = require('../src/addGuess');
 
 describe('tests addGuess function', () => {
+    
+    // jsdom()
 
+    it('tests function that adds correct guess to array in right intdex', () => {
+        word = ['t', 'e', 's', 't']
+        guess = 't'
+        correctArray = correctGuesses(word, guess)
+        assert.deepEqual(correctArray, ['t',,,'t'])
+    })
     
 })
