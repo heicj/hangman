615e3128f4f18fca838435878602bd6b99e8bc2e
Charlie <heinercj@gmail.com>
2019-03-03 16:37:54 -0800

    describe not defined when test run in debugger

--- src/correctGuesses.js
+++ src/correctGuesses.js
@@ -1,5 +1,15 @@
 function correctGuesses(word, guess){
+    let correctArray = Array(word.length).fill('');
+    let count = 0;
 
+    for(let i = 0; i < word.length; i++){
+        if(word[i] == guess){
+            correctArray.splice(i, 0, word[i])
+        }
+        count ++
+    }
+
+    return correctArray
 }
 
 module.exports = correctGuesses;
